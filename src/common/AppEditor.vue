5663b7979411a0f8fa816a7ff80e9394abd38782
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-12-25 15:46:09 +0100

    Modularize editor

--- src/common/AppEditor.vue
+++ src/common/AppEditor.vue
@@ -1,11 +1,86 @@
 <template>
-  <Editor />
+  <div class="editor">
+    <header class="editor__top-bar">
+      <p>{{ statusText }}</p>
+      <AppButton class="editor__close-btn" />
+    </header>
+
+    <Editor
+      @text-change="$emit('on-text-change', $event)"
+      @input="startTyping()"
+      class="editor__element"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-import Editor from 'primevue/editor';
+import Editor, { type EditorTextChangeEvent } from 'primevue/editor';
+import AppButton from '@/common/AppButton.vue';
+
+defineEmits<{
+  (event: 'on-text-change', text: EditorTextChangeEvent): void
+}>()
+
+import { ref } from 'vue';
+
+const statusText = ref('')
+const timerId = ref()
+const isTyping = ref(false)
+
+const startTyping = () => {
+  isTyping.value = true
+  statusText.value = 'Saving ...'
+
+  timerId.value = setTimeout(() => {
+    resetStates()
+  }, 1000);
+}
+
+const resetStates = () => {
+  statusText.value = ''
+  isTyping.value = false
+  resetTimer()
+}
+
+const resetTimer = () => {
+  clearTimeout(timerId.value)
+  timerId.value = null
+}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .editor {
+    width: 100%;
+    margin: auto;
 
+    @media (min-width: 768px) {
+      width: 768px;
+      height: 600px;
+    }
+
+    &__top-bar {
+      border: 1px solid #ccc;
+      border-bottom: unset;
+      height: 40px;
+      display: flex;
+      align-items: center;
+
+      p {
+        padding-left: 10px;
+      }
+    }
+  
+    &__close-btn {
+      margin-left: auto;
+    }
+  
+    &__element {
+      height: 100vh;
+  
+      @media (min-width: 768px) {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
 </style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,95 +1,29 @@
 <template>
   <div class="home">
-    <main class="home__editor">
-      <header>
-        <p>{{ statusText }}</p>
-        <AppButton 
-          class="home__close-btn"
-        />
-      </header>
-  
-      <AppEditor 
-        class="home__editor-element"
-        v-model="input"
-        @input="startTyping()"
-      />
-    </main>
+    <AppEditor 
+      class="home__editor" 
+      @on-text-change="saveTextChange($event)" 
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import AppButton from '@/common/AppButton.vue';
 import AppEditor from '@/common/AppEditor.vue';
-import { ref } from 'vue';
+import type { EditorTextChangeEvent } from 'primevue/editor';
 
-const input = ref('')
-const statusText = ref('')
-const timerId = ref()
-const isTyping = ref(false)
-
-const saveTextChange = () => {
+const saveTextChange = (event: EditorTextChangeEvent) => {
   //- Save text changes
-}
-
-const startTyping = () => {
-  isTyping.value = true
-  statusText.value = 'Saving ...'
-
-  timerId.value = setTimeout(() => {
-    resetStates()
-  }, 1000);
-}
-
-const resetStates = () => {
-  statusText.value = ''
-  isTyping.value = false
-  resetTimer()
-}
-
-const resetTimer = () => {
-  clearTimeout(timerId.value)
-  timerId.value = null
+  console.log('text', event.textValue);
 }
 </script>
 
 <style lang="scss" scoped>
   .home {
     display: flex;
-    height: 100vh;
+    height: 100vh;    
 
     &__editor {
-      width: 100%;
       margin: auto;
-
-      @media (min-width: 768px) {
-        width: 768px;
-        height: 600px;
-      }
-
-      header {
-        border: 1px solid #ccc;
-        border-bottom: unset;
-        height: 40px;
-        display: flex;
-        align-items: center;
-
-        p {
-          padding-left: 10px;
-        }
-      }
-    }
-
-    &__close-btn {
-      margin-left: auto;
-    }
-
-    &__editor-element {
-      height: 100vh;
-
-      @media (min-width: 768px) {
-        width: 100%;
-        height: 100%;
-      }
     }
   }
 </style>
