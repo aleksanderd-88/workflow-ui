1c7d9c5a0c5a2ad81965145dda21678b2d0d22f0
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-12-25 14:43:05 +0100

    Implement editor

---
+++ src/common/AppButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button 
+    class="app-btn" 
+    :type="type"
+  >
+    <slot />
+    <i 
+      :class="`pi pi-${ icon }`" 
+      style="font-size: 1.5rem" 
+      v-if="icon">
+    </i>
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { ButtonHTMLAttributes, PropType } from 'vue';
+
+defineProps({
+  type: {
+    type: String as PropType<ButtonHTMLAttributes['type']>,
+    default: 'button'
+  },
+
+  icon: {
+    type: String,
+    default: 'times'
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/common/AppEditor.vue
@@ -0,0 +1,11 @@
+<template>
+  <Editor />
+</template>
+
+<script setup lang="ts">
+import Editor from 'primevue/editor';
+</script>
+
+<style scoped>
+
+</style>
