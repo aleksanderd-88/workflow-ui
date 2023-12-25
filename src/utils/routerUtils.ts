065d70ef7438cd1ab724f48c4eb620b925405c7b
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-12-26 00:21:41 +0100

    Add additional function validation

--- src/utils/routerUtils.ts
+++ src/utils/routerUtils.ts
@@ -1,5 +1,9 @@
 export const setPageTitle = (title: string) => {
+  if ( typeof title === 'undefined' || !title )
+    return console.warn(`Title is missing and is required for this route.`);
+  
   if ( typeof title !== 'string' )
     return console.warn(`Wrong type! Type of "${ typeof title }" should be a "string"`);
+
   document.title = `Workflow - ${ title }`
 }
